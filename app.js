a8f80db4adf938a5a453bbbe6c31ffa196c9cb91
JON J <gr8fuldwg@gmail.com>
2018-10-18 20:35:39 -0400

    created basic clock functionality utililzing Date and setInterval

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
-console.log("it works!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!");
+// variable declrations
+const clock = document.querySelector('#clock');
+const display = clock.querySelector('#display');
+const hours = display.querySelector('#hours');
+const minutes = display.querySelector('#minutes');
+let time = new Date();
+// event bindings
+
+// functions
+function tick() {
+    const minValue = time.getMinutes();
+    const hourValue = time.getHours();
+    hours.textContent = hourValue;
+    minutes.textContent = `${minValue}`.padStart(2, '0');
+    time = new Date();
+    
+}
+
+//kick it off
+tick();
+setInterval(tick, 1000);
